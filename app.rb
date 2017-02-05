b954bf11c8785cf97a61ffca4dd3d610dabfa36c
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 12:27:41 -0600

    Drop Rdio mentions from Sinatra app

--- app.rb
+++ app.rb
@@ -3,15 +3,11 @@ require 'sinatra'
 require 'logger'
 require 'json'
 require 'omniauth'
-require 'omniauth-rdio'
-require 'rdio'
 require 'digest'
 require 'open-uri'
 require 'nokogiri'
 require_relative 'backend/google_playlist'
 
-RDIO_API_KEY = ENV['RDIO_API_KEY']
-RDIO_API_SHARED_SECRET = ENV['RDIO_API_SHARED_SECRET']
 LASTFM_API_URL = 'http://ws.audioscrobbler.com/2.0/'
 
 enable :sessions, :logging
@@ -31,21 +27,8 @@ else
   set :bower_root, File.dirname(__FILE__)
 end
 
-def get_rdio_client session
-  client = Rdio::Client.new(RDIO_API_KEY, RDIO_API_SHARED_SECRET)
-  if session[:rdio_access_token] && session[:rdio_access_secret]
-    client.access_token = session[:rdio_access_token]
-    client.secret = session[:rdio_access_secret]
-  end
-  client
-end
-
 def strip_smart_quotes str
   str.gsub(/[‘’]/, "'").gsub(/[”“]/, '"')
-end
-
-use OmniAuth::Builder do
-  provider :rdio, RDIO_API_KEY, RDIO_API_SHARED_SECRET
 end
 
 use Rack::Static, urls: ['/styles', '/scripts/controllers', '/scripts/services',
@@ -102,13 +85,6 @@ post '/google/playlist' do
   response.body.to_json
 end
 
-get '/logout/rdio' do
-  session[:rdio_user] = nil
-  session[:rdio_access_token] = nil
-  session[:rdio_access_secret] = nil
-  redirect '/index.html#/logged-out/rdio'
-end
-
 get '/auth/:name/callback' do
   # See https://github.com/nixme/omniauth-rdio for description of object
   auth = request.env['omniauth.auth']
@@ -125,44 +101,4 @@ get '/auth/failure' do
                        Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
   strategy = params[:rdio]
   redirect "/index.html#/auth/failure/#{strategy}/#{message}"
-end
-
-get '/rdio/search/artist' do
-  content_type :json
-  client = get_rdio_client(session)
-  query = strip_smart_quotes(params[:query])
-  artists = client.search(query, 'Artist')
-  if artists.count > 0
-    artist = artists[0]
-    {id: artist.key, name: artist.name}.to_json
-  else
-    {id: nil, error: "Could not find artist '#{query}' on Rdio."}.to_json
-  end
-end
-
-get '/rdio/search/track' do
-  content_type :json
-  client = get_rdio_client(session)
-  query = strip_smart_quotes(params[:query])
-  artist_id = params[:artist_id]
-  tracks = client.getTracksForArtist(artist: artist_id, query: query)
-  if tracks.count > 0
-    track = tracks[0]
-    {id: track.key}.to_json
-  else
-    {id: nil, error: "Could not find track '#{query}' on Rdio."}.to_json
-  end
-end
-
-post '/rdio/playlist' do
-  content_type :json
-  json_params = JSON.parse(request.body.read)
-  name = json_params['name']
-  description = json_params['description']
-  tracks = json_params['tracks']
-  client = get_rdio_client(session)
-  playlist = client.createPlaylist(name: name, description: description,
-                                   tracks: tracks)
-  {name: playlist.name, song_count: playlist.length, image_url: playlist.icon,
-   id: playlist.key, url: playlist.shortUrl}.to_json
 end
